45be640eed8daf6117e7b32f37da9c9ed2c40417
Wal Monfortt <jose.gual.monfortte.f@gmail.com>
2024-12-25 20:10:38 -0600

    add esho project

---
+++ eshop/src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import Header from './Header';
+import Home from './Home';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Checkout from './Checkout';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/checkout" element={
+            <>
+            <Header />
+            <Checkout />
+            </>
+          }/>
+          <Route path="/"
+          element={
+            <>
+            <Header />
+            <Home />
+            </>
+            
+          }/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
